653f24b8b46ff40ef9a7a294dcc2d4f5b12da663
mazufik <taufikprincecode10@gmail.com>
2024-02-01 22:47:31 +0900

    Setup Project

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not load environment variables", err)
+	}
+	fmt.Printf("Start Server " + os.Getenv("PORT") + "\n")
+
+	server := &http.Server{
+		Addr:           ":" + os.Getenv("PORT"),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	server_err := server.ListenAndServe()
+	if server_err != nil {
+		panic(server_err)
+	}
+}
